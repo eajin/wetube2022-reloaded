fe2fd14f14c6e1a34e59dfb52d96b7b4e4826a59
kjin <iyneed4573@gmail.com>
2022-07-04 23:02:45 +0900

    4.2 Partials

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,8 @@
+/* globalRouter */
 export const trending = (req, res) => res.render("home");
-export const see = (req, res) => {
-  return res.send(`Watch Video #${req.params.id}`);
-};
-export const edit = (req, res) => {
-  return res.send(`Edit #${req.params.id}`);
-};
 export const search = (req, res) => res.send("Search");
+/* videoRouter.js */
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
+export const deleteVideo = (req, res) => res.send("deleteVideo");
 export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => {
-  return res.send(`Delete Video #${req.params.id}`);
-};
