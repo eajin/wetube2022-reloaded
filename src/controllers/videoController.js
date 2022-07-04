fa5bb2f90dc4b16257e02a9dd88f65247daf3f19
kjin <iyneed4573@gmail.com>
2022-07-04 23:14:41 +0900

    4.4 Variables to Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,8 @@
 /* globalRouter */
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
 export const search = (req, res) => res.send("Search");
 /* videoRouter.js */
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const deleteVideo = (req, res) => res.send("deleteVideo");
 export const upload = (req, res) => res.send("Upload");
