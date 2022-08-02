b8233e96688bece993ca9ba363fbfa339d568e70
kjin <iyneed4573@gmail.com>
2022-08-02 16:08:41 +0900

    6.9 Logged In User part Two

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
@@ -28,6 +29,7 @@ app.use((req, res, next) => {
   });
 });
 
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
